76e134199a57ae3001e7650e2e5a517e80d82f20
Luiz Eduardo <luizeduardomelo.leite@gmail.com>
2022-03-15 19:59:20 -0300

    angular-ativ-5

--- src/app/btn/btn.component.ts
+++ src/app/btn/btn.component.ts
@@ -14,26 +14,47 @@ export class BtnComponent {
   AdicionarLista(): void{
     this.ItensLista.push(this.Lista);
     this.Lista = ""
-  }
- 
-  Storage(){
-    
+
     localStorage.setItem('ItensLista', JSON.stringify(this.ItensLista))
     let Stor = localStorage.getItem('Lista')
-    console.log('Local Storage:'+Stor)
+    
+    let storage1 = localStorage.getItem('ItensLista')
+    let arr1 = JSON.parse(storage1 || '[]')
+    this.ItensLista = arr1
+    
   }
+  recuperarstorage():void{
+    let storage1 = localStorage.getItem('ItensLista')
+    let arr1 = JSON.parse(storage1 || '')
+    this.ItensLista = arr1
+    //this.Storage = localStorage.getItem('dado')
+
+  }
+  
+ 
+ 
 
   
   @Output()
   clicou2:EventEmitter<any> = new EventEmitter<any>()
   Clear(){
     this.ItensLista.splice(this.Lista.length)
+    
+    this.ItensLista.splice(this.ItensLista.length)
+      let arr1 = JSON.stringify(this.ItensLista)
+      localStorage.setItem('ItensLista',arr1)
+    
     this.clicou2.emit()
   }
   @Output()
   clicou1:EventEmitter<string> = new EventEmitter<string>()
   RemoverLista(Itens:string){
       this.ItensLista.splice(this.ItensLista.indexOf(Itens),1)
+      
+      this.ItensLista.splice(this.ItensLista.indexOf(Itens),1)
+      let arr2 = JSON.stringify(this.ItensLista)
+      localStorage.setItem('ItensLista',arr2)
+
       this.clicou1.emit()
     }
   }
